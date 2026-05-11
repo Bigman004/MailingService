486ef6795c67d3150ce6c3e6b9d1214d57230a21
bigman004 <adedijiadetunji228@gmail.com>
2026-05-11 14:57:11 +0100

    changes

--- src/main/java/com/example/mailingService/controller/MailController.java
+++ src/main/java/com/example/mailingService/controller/MailController.java
@@ -10,55 +10,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
 public class MailController {
     @Autowired
     private MailingService mailingService;
+
     @GetMapping("/welcome")
-    public String welcome(){
+    public String welcome() {
         return "Welcome to mailing service!";
 
     }
+
     @GetMapping("/send_mail")
-    public ResponseEntity<?> sendMail(){
+    public ResponseEntity<?> sendMail() {
         return new ResponseEntity<>(MailDto.builder().build(), HttpStatus.OK);
     }
 
     @PostMapping("/send_mail")
-    public ResponseEntity<?> sendMail(@RequestBody MailDto mailDto){
+    public ResponseEntity<?> sendMail(@RequestBody MailDto mailDto) {
         log.info("mail received... mailing service");
-        try{
+        try {
             mailingService.sendHtmlMail(mailDto);
             return new ResponseEntity<>("mail has been sent to server", HttpStatus.OK);
-        }
-        catch(MessagingException e){
+        } catch (MessagingException e) {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
     }
+
     @PostMapping("/send_reciept")
-    public ResponseEntity<?> sendReciept(@RequestBody OrderDto orderDto){
+    public ResponseEntity<?> sendReciept(@RequestBody OrderDto orderDto) {
         log.info("order received... mailing service");
 
-        try{
+        try {
             mailingService.sendReciept(orderDto);
             return new ResponseEntity<>("receipt has been sent to recipient", HttpStatus.OK);
-        }
-        catch(MessagingException e){
+        } catch (MessagingException e) {
             e.printStackTrace();
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
     @GetMapping("/ping")
-    public ResponseEntity<?> ping(){
+    public ResponseEntity<?> ping() {
         return new ResponseEntity<>("pong", HttpStatus.OK);
     }
-
 }
+
